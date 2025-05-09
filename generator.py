bc12aaaf015bc26b473bd2547064ca8623f4bf4c
skaffa <59300989+skaffa@users.noreply.github.com>
2025-05-09 06:22:20 +0200

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,7 +1,9 @@
 import random
 
 def generate_valid_bonuskaart():
-    prefix = "2623030"  # 7 fixed digits
+    valid_prefixes = [(2621100,2621140), (2622000,2622030), (2622200,2622270), (2623013,2623036)]
+    random_range = random.choice(valid_prefixes)
+    prefix = str(random.randint(random_range[0], random_range[1]))
     body = ''.join(str(random.randint(0, 9)) for _ in range(5))  # 5 random digits to make 12 total
     partial = prefix + body
 
@@ -14,5 +16,5 @@ def generate_valid_bonuskaart():
     check_digit = (10 - (total % 10)) % 10
     return partial + str(check_digit)
 
-# Example usage:
+
 print(generate_valid_bonuskaart())
